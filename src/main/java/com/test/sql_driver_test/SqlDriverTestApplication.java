9b1a4646515e674418b50b36acb3c136d6f75e27
ren_xt <allbutone@foxmail.com>
2017-07-20 15:23:44 +0800

    initial commit

---
+++ src/main/java/com/test/sql_driver_test/SqlDriverTestApplication.java
@@ -0,0 +1,12 @@
+package com.test.sql_driver_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlDriverTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlDriverTestApplication.class, args);
+	}
+}
